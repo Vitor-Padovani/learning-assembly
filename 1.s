cde03ce67c54b7955a8a25f03bd5f446ddbdf37f
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2026-03-31 10:42:03 +0000

    exercise 1

--- 1.s
+++ 1.s
@@ -2,26 +2,43 @@
 
 .section .data
     num:    .long 0
-    fmt:    .asciz "%d"
-    msg:    .asciz "Type a number: "
+    fmt_in:  .asciz "%d"
+    less_out: .asciz "\npredecessor: %d\n"
+    more_out: .asciz "\nsucessor: %d\n"
+    msg:    .asciz "\ntype a number: "
 
 .section .text
 
 main:
-    sub $8, %rsp # Stack aligment
+    sub $8, %rsp
 
-    # Scanf
-    lea fmt(%rip), %rdi
+    # printf("type a number: ")
+    lea msg(%rip), %rdi
+    xor %rax, %rax
+    call printf
+
+    # scanf("%d", &num)
+    lea fmt_in(%rip), %rdi
     lea num(%rip), %rsi
     xor %rax, %rax
     call scanf
 
-    # Printf
-    lea fmt(%rip), %rdi
+    sub $1, num(%rip)
+
+    # printf("numero: %d\n", num)
+    lea less_out(%rip), %rdi
     mov num(%rip), %rsi
     xor %rax, %rax
     call printf
 
-    add $8, %rsp # Stack aligment
-    mov $0, %rax
+    add $2, num(%rip)
+
+    # printf("numero: %d\n", num)
+    lea more_out(%rip), %rdi
+    mov num(%rip), %rsi
+    xor %rax, %rax
+    call printf
+
+    add $8, %rsp
+    mov $0, %eax
     ret
